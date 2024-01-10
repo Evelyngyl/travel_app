57231cb353907ce619a05dc61eacd70fc29420c0
Evelyngyl <934336856@qq.com>
2024-01-10 17:46:10 -0600

    out

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,12 +1,13 @@
 import { PEOPLE_URL } from "@/constants";
 import Image from "next/image";
+
 interface CampProps {
   backgroundImage: string;
   title: string;
   subtitle: string;
   peopleJoined: string;
 }
-const CampSite = ({backgroundImage, title, subtitle, peopleJoined}) =>{
+const CampSite = ({backgroundImage, title, subtitle, peopleJoined}: CampProps) =>{
   return (
     <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
       <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">

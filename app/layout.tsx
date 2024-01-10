14fdb15d1bcf3e8e654d789b759129e5944842cf
Evelyngyl <934336856@qq.com>
2024-01-10 11:40:31 -0600

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Navbar from '@/components/Navbar'
+import { Children } from 'react'
+import Footer from '@/components/Footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Travel',
+  description: 'Travel UI/UX app for camping',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className='relative overflow-hidden'>
+        {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
